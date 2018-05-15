89f17c7eb00866e00eac52ab32e26d0a95939f1a
Matt Sell <mwsell@gmail.com>
2018-05-14 22:04:06 -0500

    create resolver for violations

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,6 +1,19 @@
 /* eslint no-unused-vars: 0 */
+import axios from 'axios';
+
 export default {
   Query: {
-    violations: (parent, args, ctx, info) => 'hi',
+    allViolations: async (parent, args, ctx, info) => {
+      const res = await axios.get('https://cpsc-api.herokuapp.com/api/violations');
+
+      return {
+        total: res.data.total,
+        violations: res.data.violations.map(x => ({
+          id: x.id,
+          total: x.total,
+          description: x.violation,
+        })),
+      };
+    },
   },
 };
--- server/schema.js
+++ server/schema.js
@@ -1,5 +1,22 @@
 export default `
+  type Infraction {
+    date: String!
+    product: String!
+    violation: String!
+    firm: String!
+  }
+
+  type Violation {
+    total: Int!
+    id: Int!
+    description: String!    
+  }
+
+  type ViolationInfo {
+    total: Int!
+    violations: [Violation]!
+  }
   type Query {
-    violations: String
+    allViolations: ViolationInfo!
   } 
 `;
