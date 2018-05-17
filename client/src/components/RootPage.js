c01a0689749da52fcb7d1f08475df11b830efb72
Matt Sell <msell@onetechnologies.net>
2018-05-17 16:32:10 -0500

    make selectable list

--- client/src/components/RootPage.js
+++ client/src/components/RootPage.js
@@ -1,76 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import {
-  Table,
-  TableBody,
-  TableHeader,
-  TableHeaderColumn,
-  TableRow,
-  TableRowColumn,
-} from 'material-ui/Table';
-import { Query } from 'react-apollo'
-import getViolations from '../queries/getViolations'
+import React from "react";
+import { Link } from "react-router-dom";
+import {List, ListItem, makeSelectable} from 'material-ui/List';
+import { Query } from "react-apollo";
+import getViolations from "../queries/getViolations";
+
+let SelectableList = makeSelectable(List);
 
 class RootPage extends React.Component {
-  state = {ids: [{id: 4}, {id: 20}]}
+  state = { ids: [{ id: 4 }, { id: 20 }] };
   render() {
     return (
-      <div><h1>Root Page</h1>
-      <Query query={getViolations}>
-        {({ loading, error, data }) => {
-          if(loading) return "Loading...";
-          if(error) return `Error! ${error.message}`
+      <div>
+        <h1>Violations</h1>
+        <Query query={getViolations}>
+          {({ loading, error, data }) => {
+            if (loading) return "Loading...";
+            if (error) return `Error! ${error.message}`;
 
-          return (
-            data.allViolations.violations.map(((x, i) => (<div key={i}>{x.description}</div>)))
-          )
-        }}
-      </Query>
-      <Table>
-    <TableHeader>
-      <TableRow>
-        <TableHeaderColumn>ID</TableHeaderColumn>
-        <TableHeaderColumn>Name</TableHeaderColumn>
-        <TableHeaderColumn>Status</TableHeaderColumn>
-      </TableRow>
-    </TableHeader>
-    <TableBody>
-      <TableRow>
-        <TableRowColumn>1</TableRowColumn>
-        <TableRowColumn>John Smith</TableRowColumn>
-        <TableRowColumn>Employed</TableRowColumn>
-      </TableRow>
-      <TableRow>
-        <TableRowColumn>2</TableRowColumn>
-        <TableRowColumn>Randal White</TableRowColumn>
-        <TableRowColumn>Unemployed</TableRowColumn>
-      </TableRow>
-      <TableRow>
-        <TableRowColumn>3</TableRowColumn>
-        <TableRowColumn>Stephanie Sanders</TableRowColumn>
-        <TableRowColumn>Employed</TableRowColumn>
-      </TableRow>
-      <TableRow>
-        <TableRowColumn>4</TableRowColumn>
-        <TableRowColumn>Steve Brown</TableRowColumn>
-        <TableRowColumn>Employed</TableRowColumn>
-      </TableRow>
-      <TableRow>
-        <TableRowColumn>5</TableRowColumn>
-        <TableRowColumn>Christopher Nolan</TableRowColumn>
-        <TableRowColumn>Unemployed</TableRowColumn>
-      </TableRow>
-    </TableBody>
-  </Table>
-      <ul>
-      {        
-        this.state.ids.map((x) => <li key={x.id}><Link to={`/${x.id}`}>Go here: {x.id}</Link></li>)
-      }
+            return (
+              <SelectableList>                                
+                  {data.allViolations.violations.map(x => (                    
+                      <ListItem value={x.id} key={x.id} primaryText={`${x.description} (${x.total})`} />
+                  ))}                
+              </SelectableList>
+            );
+          }}
+        </Query>
+
+        <ul>
+          {this.state.ids.map(x => (
+            <li key={x.id}><Link to={`/${x.id}`}>Go here: {x.id}</Link></li>
+          ))}
         </ul>
-    </div>
-    )
+      </div>
+    );
   }
 }
 
 export default RootPage;
-
--- client/src/queries/getViolations.js
+++ client/src/queries/getViolations.js
@@ -4,7 +4,9 @@ export default gql`
   {
   allViolations {
     violations {
+      id
       description
+      total
     }
   }
 }`;
